bddb777133fd4c73d765283c114a55f838c1d4ba
Mark Fisher <mark.aaron.fisher@gmail.com>
2017-05-11 10:50:40 -0700

    add auth-component component, set up firebase to link to kitty corruption app, and init firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {masterFirebaseConfig} from './api-keys';
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { AuthComponentComponent } from './auth-component/auth-component.component';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
